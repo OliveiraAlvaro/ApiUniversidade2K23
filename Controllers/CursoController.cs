70cacbbe2bf81834b026a0e9c055d0efed8951e8
Alvaro <alvaro-henrique111@hotmail.com>
2023-10-24 11:35:38 -0300

    dor de cabeca

---
+++ Controllers/CursoController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using apiUniversidade.Model;
+using apiUniversidade.Context;
+
+
+namespace apiUniversidade.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+
+   
+    public class CursoController : ControllerBase
+    {
+        private readonly ILogger<CursoController> _logger;
+        private readonly apiUniversidadeContext _context;
+
+
+         public CursoController(ILogger<CursoController> logger, apiUniversidadeContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Curso>> Get()
+        {
+            var cursos = _context.Cursos.ToList();
+            if(cursos is null)
+                return NotFound();  
+
+
+            return cursos;
+        }
+
+
+        [HttpPost]
+        public ActionResult Post(Curso curso){
+            _context.Cursos.Add(curso);
+            _context.SaveChanges();
+
+
+            return new CreatedAtRouteResult ("GetCurso", new{ id = curso.ID}, curso);
+        }
+    }
+}
+
+
+
+    
