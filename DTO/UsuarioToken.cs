30361269dea4e8ecda52ae47eaec15c482237c3f
Alvaro <alvaro-henrique111@hotmail.com>
2023-12-05 11:58:48 -0300

    atualizando o dto e token

---
+++ DTO/UsuarioToken.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace APIUNIVERSIDADE.DTO
+{
+    public class UsuarioToken
+    {
+        public bool Athenticated {get; set;}
+        public DateTime Expiration {get; set;}
+        public string Token {get; set;}
+        public string Message {get; set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,29 @@
 using apiUniversidade.Context;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 var connectionString = builder.Configuration.GetConnectionString("PostgreSQL");
 builder.Services.AddDbContext<apiUniversidadeContext>(options => options.UseNpgsql(connectionString));
 
+
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<apiUniversidadeContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -22,10 +32,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
+
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
+
 app.Run();
+
